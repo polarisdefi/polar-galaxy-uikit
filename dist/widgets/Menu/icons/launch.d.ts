20b11cfcd2949c111a6a573b45c88ebed39793d6
Polaris Defi <80004555+polarisdefi@users.noreply.github.com>
2021-03-27 03:34:55 +1000

    Delete dist directory

--- dist/widgets/Menu/icons/launch.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SvgProps } from "../../../components/Svg/types";
-interface LogoProps extends SvgProps {
-    isDark: boolean;
-}
-declare const Launch: React.FC<LogoProps>;
-export default Launch;
